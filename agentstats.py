c619edbeface2ef6825be8cf4f70a13442be98b6
Willem <willem2008@outlook.com>
2020-07-28 21:20:33 +0200

    Minor fix and new test graph added

--- agentstats.py
+++ agentstats.py
@@ -120,6 +120,13 @@ def init_db():
 	conn.close()
 
 
+def make_patch_spines_invisible(ax):
+    ax.set_frame_on(True)
+    ax.patch.set_visible(False)
+    for sp in ax.spines.values():
+        sp.set_visible(False)
+
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	cid = message.chat.id
@@ -247,6 +254,7 @@ def send_stats(message):
 	except:
 		pass
 
+
 @bot.message_handler(commands=['test', 'help'])
 def send_test(message):
 	cid = message.chat.id
@@ -269,6 +277,8 @@ def send_test(message):
 	plt.xticks(stats_date, color='b', rotation=45)
 	plt.ylabel("XM Recharged", color='b')
 	plt.xlabel("Time", color='b', rotation=45)
+	plt.tight_layout()
+
 	plt.savefig('images/{}.png'.format(cid))
 	img = open('images/{}.png'.format(cid), 'rb')
 	log.debug("Before send test")
@@ -278,6 +288,88 @@ def send_test(message):
 		os.remove('images/{}.png'.format(cid))
 	except:
 		pass
+
+
+@bot.message_handler(commands=['test1', 'help'])
+def send_test1(message):
+	cid = message.chat.id
+	agent_telegram_id = message.from_user.id
+	conn = sqlite3.connect('agent_stats.db')
+	c = conn.cursor()
+
+	player_ap = []
+	player_recharged = []
+	player_resonator_deployed = []
+	player_links_created = []
+	player_control_field_created = []
+
+	stats_date = []
+	for row in c.execute("SELECT * FROM agents_stats WHERE agent_telegram_id={}".format(agent_telegram_id)):
+		# print(row)   
+		# stats_date.append(row[4])
+		stats_date.append('{} {}'.format(row[4], row[5]))
+		player_ap.append(row[8])
+		# player_uniekportal.append(row[9])
+		player_resonator_deployed.append(row[17])
+		player_links_created.append(row[18])
+		player_control_field_created.append(row[19])
+		player_recharged.append(row[24])
+	conn.close()
+	player_name = row[2]
+
+	fig, host = plt.subplots()
+	fig.subplots_adjust(right=0.75)
+
+	par1 = host.twinx()
+	par2 = host.twinx()
+
+	# Offset the right spine of par2.  The ticks and label have already been
+	# placed on the right by twinx above.
+	par2.spines["right"].set_position(("axes", 1.2))
+	# Having been created by twinx, par2 has its frame off, so the line of its
+	# detached spine is invisible.  First, activate the frame but make the patch
+	# and spines invisible.
+	make_patch_spines_invisible(par2)
+	# Second, show the right spine.
+	par2.spines["right"].set_visible(True)
+
+	p1, = host.plot(stats_date, player_ap, "b-", label="Player AP")
+	p2, = par1.plot(stats_date, player_links_created, "r-", label="Linkes created")
+	p3, = par2.plot(stats_date, player_control_field_created, "g-", label="Control field created")
+
+	host.tick_params('x', labelrotation=90)
+
+	par1.set_ylim(0, max(player_links_created))
+	par2.set_ylim(0, max(player_control_field_created))
+
+	host.set_xlabel("Time")
+	host.set_ylabel("Player AP")
+	par1.set_ylabel("Linkes created")
+	par2.set_ylabel("Control field created")
+
+	host.yaxis.label.set_color(p1.get_color())
+	par1.yaxis.label.set_color(p2.get_color())
+	par2.yaxis.label.set_color(p3.get_color())
+
+	tkw = dict(size=4, width=1.5)
+	host.tick_params(axis='y', colors=p1.get_color(), **tkw)
+	par1.tick_params(axis='y', colors=p2.get_color(), **tkw)
+	par2.tick_params(axis='y', colors=p3.get_color(), **tkw)
+	host.tick_params(axis='x', **tkw)
+
+	lines = [p1, p2, p3]
+
+	host.legend(lines, [l.get_label() for l in lines])
+	plt.tight_layout()
+	plt.show()
+	plt.savefig('images/{}.png'.format(cid))
+	img = open('images/{}.png'.format(cid), 'rb')
+	bot.send_photo(cid, img)
+	try:
+		os.remove('images/{}.png'.format(cid))
+	except:
+		pass
+
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
